dd24697a4f888ab8262e49e86187ba79a079a0ad
Sh-a-wn <senkungushawn@gmail.com>
2024-07-05 07:34:06 +0300

    update animation and app name

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -1,14 +1,47 @@
+import 'package:course_correct/pages/landing_page.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class TestPage extends StatelessWidget {
+class TestPage extends StatefulWidget {
   const TestPage({super.key});
+
+  @override
+  _TestPageState createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 8), () {
+      // Navigate to a new page 
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LandingPage()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Lottie.asset('assets/animations/course correct.json'),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          SizedBox(height: 120,),
+          Center(
+            child: Lottie.asset('assets/animations/course correct.json'),
+          ),
+          SizedBox(height: 20,),
+          Text(
+            "Course Correct",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          )
+        ],
       ),
     );
   }
