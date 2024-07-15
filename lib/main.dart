e26acc4c25d483dac957dca2273cfff9ea5feddc
Okello-Arnold <okelloarnold4@gmail.com>
2024-07-15 18:29:52 +0300

    imported packages for the new pages created

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:course_correct/appstate/app_state.dart';
+import 'package:course_correct/pages/appointments_page.dart';
 import 'package:course_correct/pages/landing_page.dart';
+import 'package:course_correct/pages/tutor_appointments.dart';
+import 'package:course_correct/pages/tutor_students.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:course_correct/pages/profile_page.dart';
 import 'package:course_correct/pages/test_page.dart';
 import 'package:provider/provider.dart';
+
 
 late AppState appState;
 void main() async {
@@ -28,6 +32,10 @@ class MyApp extends StatelessWidget {
         routes: {
           '/profilepage': (context) => const ProfilePage1(),
           '/landingpage': (context) => LandingPage(),
+          '/appointments': (context) => TutorAppointments(),
+          '/students': (context) => TutorStudents(),
+          // ignore: equal_keys_in_map
+          '/appointments': (context) => AppointmentsPage(),
           // '/settingspage': (context) => SettingsPage(),
         },
         theme: ThemeData(
