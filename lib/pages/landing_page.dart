e4616ee8206eaecddb4fbcb3c7367972dabbccb7
Julianexe <julianssonko@gmail.com>
2024-07-25 00:56:41 +0300

    chore: Update landing page to dynamically display homepage based on user role
    This commit updates the landing page to dynamically display the appropriate homepage based on the user's role. If the user is a tutor, the tutor homepage is shown. If the user is a student, the student homepage is shown. This improves the user experience by providing personalized content for each role.
    
    Closes #15

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -2,6 +2,8 @@
 
 import 'package:course_correct/main.dart';
 import 'package:course_correct/pages/initial_selection_page.dart';
+import 'package:course_correct/pages/student_homepage.dart';
+import 'package:course_correct/pages/tutors_homepage.dart';
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatelessWidget {
@@ -9,76 +11,10 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Course ",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              "Correct ",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-        backgroundColor: Colors.black87,
-        elevation: 50,
-        iconTheme: IconThemeData(color: Colors.lightBlueAccent),
-      ),
-      drawer: Drawer(
-        child: Container(
-          color: Colors.grey[100],
-          child: Column(
-            children: [
-              DrawerHeader(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    iconColor: Colors.blueAccent, // background color
-                    backgroundColor: Colors.white, // foreground color
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.pushNamed(context, '/profilepage');
-                  },
-                  child: Icon(
-                    Icons.person,
-                    size: 45,
-                  ),
-                ),
-              ),
-              // Categories with Dropdown
-              
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text("Settings"),
-                onTap: () {
-                  Navigator.pop(context);
-                  // go to settings page
-                  Navigator.pushNamed(context, '/settingspage');
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.logout),
-                title: Text("Logout"),
-                onTap: () {
-                  //log user out
-                 appState.logoutUser(context);
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-      
-      body: RoleSelectionPage(),
-    );
+    return 
+      appState.userProfile!.role == 'tutor' ? TutorHomepage(): appState.userProfile!.role == 'student' ? StudentHomepage() : RoleSelectionPage();
+    
   }
+
+ 
 }
