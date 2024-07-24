4537c77c79139a26d9c3e0a1855d852f7dfdf02b
Julianexe <julianssonko@gmail.com>
2024-07-25 00:34:20 +0300

    feat: Add user registration and login functionality
    This commit adds the functionality for user registration and login. It includes methods for registering a tutor on Firestore, getting a tutor's information, getting all tutors, and adding a user to Firestore. It also includes the login sequence for authenticating users and redirecting them to the appropriate homepage based on their role. Additionally, it refactors the `initialization` method to set the user and user profile, and adds a `setUserProfile` method to update the user profile.
    
    Closes #15

--- lib/appstate/app_state.dart
+++ lib/appstate/app_state.dart
@@ -1,18 +1,21 @@
 //change notifier class
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:course_correct/models/tutor_model.dart';
 import 'package:course_correct/models/user_model.dart';
+import 'package:course_correct/pages/landing_page.dart';
 import 'package:course_correct/pages/login_page.dart';
+import 'package:course_correct/pages/student_homepage.dart';
+import 'package:course_correct/pages/tutors_homepage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
 class AppState extends ChangeNotifier {
   bool animationcomplete = false;
 
   //variable to store the current user
   User? user;
+  UserModel? userProfile;
   //variable to store the current theme
   ThemeData _theme = ThemeData.light();
   //getter to get the current theme
@@ -28,14 +31,15 @@ class AppState extends ChangeNotifier {
     notifyListeners();
   }
 
-
-  Future<void> initialization()async{
-
-    user = FirebaseAuth.instance.currentUser;
-    notifyListeners();
+  Future<void> initialization() async {
+    setUser(FirebaseAuth.instance.currentUser);
+    setUserProfile(await readUserProfileFromFirestore());
   }
 
-
+  void setUser(User? user) {
+    this.user = user;
+    notifyListeners();
+  }
 
   void logoutUser(BuildContext context) {
     FirebaseAuth.instance.signOut();
@@ -45,12 +49,20 @@ class AppState extends ChangeNotifier {
         context, MaterialPageRoute(builder: (context) => const LoginPage()));
   }
 
-  void registerTutorOnFirestore(TutorModel tutor){
-    FirebaseFirestore.instance.collection('tutors').doc(user!.uid).set(tutor.toFirestore());
+  void registerTutorOnFirestore(TutorModel tutor) {
+    FirebaseFirestore.instance
+        .collection('tutors')
+        .doc(user!.uid)
+        .set(tutor.toFirestore());
   }
 
-  Future<TutorModel?> getTutor()async{
-    final tutorRef = FirebaseFirestore.instance.collection('tutors').doc(user!.uid).withConverter(fromFirestore:TutorModel.fromFirestore , toFirestore: (TutorModel tutor, _) =>tutor.toFirestore());
+  Future<TutorModel?> getTutor() async {
+    final tutorRef = FirebaseFirestore.instance
+        .collection('tutors')
+        .doc(user!.uid)
+        .withConverter(
+            fromFirestore: TutorModel.fromFirestore,
+            toFirestore: (TutorModel tutor, _) => tutor.toFirestore());
     final tutorSnap = await tutorRef.get();
     final tutor = tutorSnap.data();
     return tutor;
@@ -58,26 +70,111 @@ class AppState extends ChangeNotifier {
 
   //function to get all tutors
   Future<List<TutorModel>> getAllTutors() async {
-    final tutorsRef = FirebaseFirestore.instance.collection('tutors').withConverter(
-      fromFirestore: (snapshot, _) => TutorModel.fromFirestore(snapshot.data()! as DocumentSnapshot<Map<String, dynamic>>,_),
-      toFirestore: (tutor, _) => tutor.toFirestore(),
-    );
+    final tutorsRef = FirebaseFirestore.instance
+        .collection('tutors')
+        .withConverter(
+          fromFirestore: (snapshot, _) => TutorModel.fromFirestore(
+              snapshot.data()! as DocumentSnapshot<Map<String, dynamic>>, _),
+          toFirestore: (tutor, _) => tutor.toFirestore(),
+        );
     final tutorsSnap = await tutorsRef.get();
     final tutors = tutorsSnap.docs.map((doc) => doc.data()).toList();
     return tutors;
   }
 
-  void usertoFirebase(bool isTutor){
-    FirebaseFirestore.instance.collection('users').doc(user!.uid).set({
-      'isTutor': isTutor,
-    });
+  void usertoFirebase(bool isTutor) {
+    FirebaseFirestore.instance.collection('users').doc(user!.uid).set({});
   }
 
-  Future<bool> isUserTutor() async {
-    var user = FirebaseAuth.instance.currentUser;
-    final userRef = FirebaseFirestore.instance.collection('users').doc(user!.uid).withConverter(fromFirestore: UserModel.fromFirestore, toFirestore: (user, _) => user.toFirestore());
-    final userSnap = await userRef.get();
-    final userModel = userSnap.data();
-    return userModel!.isTutor;
+  // Future<void> addStudentToFirestore(String uid, String name) async {
+  //   User? user = FirebaseAuth.instance.currentUser;
+  //   if (user != null){
+  //   await FirebaseFirestore.instance.collection('users').doc(uid).set({
+  //     'email': user.email,
+  //     'name': name,
+  //     'created_at': FieldValue.serverTimestamp(),
+  //     'role': 'student',
+  //   });
+  // }
+  // }
+
+  Future<void> loginSequence(
+      String email, String password, BuildContext context) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      User? user = userCredential.user;
+      if (user != null) {
+        setUser(user);
+        userProfile = await readUserProfileFromFirestore();
+        notifyListeners();
+        if (userProfile!.role == 'tutor') {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const TutorHomepage()));
+        } else if (userProfile!.role == 'student') {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const StudentHomepage()));
+        } else {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LandingPage()));
+        }
+      }
+    } catch (e) {
+      snackBarMessage(e.toString(), context);
+    }
+  }
+
+  Future<UserModel?> readUserProfileFromFirestore() async {
+    user = FirebaseAuth.instance.currentUser;
+    String userUid = user!.uid;
+    if (user != null) {
+      final userRef = FirebaseFirestore.instance
+          .collection('users')
+          .doc(userUid)
+          .withConverter(
+            fromFirestore: (snapshot, _) => UserModel.fromFirestore(
+                snapshot, _),
+            toFirestore: (user, _) => user.toFirestore(),
+          );
+      final userSnap = await userRef.get();
+      final user = userSnap.data();
+      return user;
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> registerSequence(
+      String email, String password, String name, BuildContext context) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      User? user = userCredential.user;
+      if (user != null) {
+        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+          'email': email,
+          'name': name,
+          'created_at': FieldValue.serverTimestamp(),
+        });
+        setUser(user);
+        userProfile = UserModel(name: name);
+        notifyListeners();
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LandingPage()));
+      }
+    } catch (e) {
+      snackBarMessage(e.toString(), context);
+    }
+  }
+
+  void setUserProfile(UserModel? userProfile) {
+    this.userProfile = userProfile;
+    notifyListeners();
+  }
+
+  snackBarMessage(String text, BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(text),
+    ));
   }
 }
