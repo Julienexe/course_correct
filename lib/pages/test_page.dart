ce09f578e12c2f887a3f7d30c93f4d15170db558
Julianexe <julianssonko@gmail.com>
2024-07-25 00:35:22 +0300

    chore: Update test page to include page selection

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -1,6 +1,8 @@
 import 'package:course_correct/main.dart';
 import 'package:course_correct/pages/landing_page.dart';
 import 'package:course_correct/pages/login_page.dart';
+import 'package:course_correct/pages/student_homepage.dart';
+import 'package:course_correct/pages/tutors_homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -15,16 +17,29 @@ class _TestPageState extends State<TestPage> {
   @override
   void initState() {
     super.initState();
-     Future.delayed(const Duration(seconds: 3), () async{
-        await appState.initialization();
-        //check if user exists
-        if(appState.user != null){
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LandingPage()));
-        }else{
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>const LoginPage()));
+    Future.delayed(const Duration(seconds: 3), () async {
+      await appState.initialization();
+      //check if user exists
+      if (appState.user != null) {
+        if (appState.userProfile != null) {
+          if (appState.userProfile!.role == 'tutor') {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const TutorHomepage()));
+          } else if (appState.userProfile!.role == 'student') {
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const StudentHomepage()));
+          }
         }
-        appState.animationcomplete = true;
-     });
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LandingPage()));
+      } else {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const LoginPage()));
+      }
+      appState.animationcomplete = true;
+    });
   }
 
   @override
@@ -37,7 +52,9 @@ class _TestPageState extends State<TestPage> {
           Center(
             child: Lottie.asset('assets/animations/course correct.json'),
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           const Text(
             "COURSE CORRECT",
             style: TextStyle(
