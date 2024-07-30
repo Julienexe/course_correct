d9344700400bdbe89a78e5081fccc8cd68c3698f
Julianexe <julianssonko@gmail.com>
2024-07-30 23:01:44 +0300

    implement matching algorithm

--- lib/pages/tutor_dashboard.dart
+++ lib/pages/tutor_dashboard.dart
@@ -1,3 +1,5 @@
+import 'package:course_correct/main.dart';
+import 'package:course_correct/pages/tutor_matching_algorithm.dart';
 import 'package:flutter/material.dart';
 
 class TutorDashboard extends StatelessWidget {
@@ -15,63 +17,110 @@ class TutorDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tutor Dashboard'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Student ID: $studentId',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return FutureBuilder(
+      future: appState.fetchTutors(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        //print(snapshot.data);
+        TutorModelMatcher tutorMatcher = TutorModelMatcher(tutors: 
+          snapshot.data!
+          );
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Tutor Dashboard'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Student ID: $studentId',
+                  style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Overall Score: $score/${questionsPerTopic.values.reduce((a, b) => a + b)}',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Scores by Topic:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                ...scoresByTopic.entries.map((entry) {
+                  return Text(
+                    '${entry.key}: ${entry.value}/${questionsPerTopic[entry.key]}',
+                    style: const TextStyle(fontSize: 18),
+                  );
+                }).toList(),
+                const SizedBox(height: 20),
+                const Text(
+                  'Additional Features',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    List<Map> tutorList = tutorMatcher.matchTutors(scoresByTopic);
+                    Map topTutor = tutorList[0];
+                    showDialog(
+                      context: context, 
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Recommendations'),
+                          content: Column(
+                            children:[
+                              const Text('You have been matched with the following tutor:',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
+                              ),),
+                              Text('Name: ${topTutor['Tutor']}',
+                              style: const TextStyle(
+                                fontSize: 16
+                              ),),
+                            ] 
+
+                          ),
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Close'),
+                            ),
+                          ],
+                        );
+                      }
+                      );
+                    // Add functionality for exporting the results
+                  },
+                  child: const Text('Find me a tutor'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    
+                    // Add functionality for emailing the results
+                  },
+                  child: const Text('Email Results'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add functionality for showing detailed analysis
+                  },
+                  child: const Text('Show Detailed Analysis'),
+                ),
+              ],
             ),
-            SizedBox(height: 10),
-            Text(
-              'Overall Score: $score/${questionsPerTopic.values.reduce((a, b) => a + b)}',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Scores by Topic:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            ...scoresByTopic.entries.map((entry) {
-              return Text(
-                '${entry.key}: ${entry.value}/${questionsPerTopic[entry.key]}',
-                style: TextStyle(fontSize: 18),
-              );
-            }).toList(),
-            SizedBox(height: 20),
-            Text(
-              'Additional Features',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                // Add functionality for exporting the results
-              },
-              child: Text('Export Results'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Add functionality for emailing the results
-              },
-              child: Text('Email Results'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Add functionality for showing detailed analysis
-              },
-              child: Text('Show Detailed Analysis'),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }
     );
   }
 }
