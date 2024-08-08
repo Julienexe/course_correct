fc9b75af1f378ed2ed1e319cc00baa3d6dd091e6
Okello-Arnold <okelloarnold4@gmail.com>
2024-08-08 13:34:55 +0300

    another change

--- lib/pages/tutors_homepage.dart
+++ lib/pages/tutors_homepage.dart
@@ -133,7 +133,7 @@ class _TutorHomepageState extends State<TutorHomepage> {
             ListTile(
               title: const Text('Home'),
               onTap: () {
-                // Navigate to home
+                
               },
             ),
             ListTile(
