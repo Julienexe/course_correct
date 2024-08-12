c2233bc6a4c022823aacec63f6fb67a3245fe28d
Michael Rusezera <michaelrusezera@gmail.com>
2024-08-12 23:34:28 +0300

    "Updated height of Carousel item container from 360 to 410"

--- lib/components/slider.dart
+++ lib/components/slider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 class CarouselWigdet extends StatefulWidget {
-  final List<Widget>items;
+  final List<Widget> items;
   CarouselWigdet({super.key, required this.items});
 
   @override
@@ -10,12 +10,11 @@ class CarouselWigdet extends StatefulWidget {
 }
 
 class _CarouselWigdetState extends State<CarouselWigdet> {
-  
   @override
   Widget build(BuildContext context) {
     return CarouselSlider.builder(
       options: CarouselOptions(
-       height: 430,
+        height: 500,
         enlargeCenterPage: true,
         enableInfiniteScroll: true,
         autoPlay: true,
