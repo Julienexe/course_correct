1b86755f2b1fce9058432d45ca2e9f73002850b8
Michael Rusezera <michaelrusezera@gmail.com>
2024-08-13 00:03:49 +0300

    read appointments from db

--- lib/pages/tutors/appointments.dart
+++ lib/pages/tutors/appointments.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:course_correct/main.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentsPage extends StatelessWidget {
@@ -7,12 +9,36 @@ class AppointmentsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Appointments'),
       ),
-      body: Center(
-        child: Text(
-          'Coming Soon...',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+      body: FutureBuilder(
+          future: getBookings(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('Appointment'),
+                      subtitle: Column(
+                        children: [
+                          Text(snapshot.data?[index]['startTime']),
+                          Text(snapshot.data?[index]['endTime']),
+                        ],
+                      ),
+                    );
+                  });
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
+}
+
+Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> getBookings() async {
+  final db = FirebaseFirestore.instance;
+  var bookings = await db
+      .collection('bookings')
+      .where('tutorId', isEqualTo: appState.user?.uid)
+      .get();
+  return bookings.docs;
 }
