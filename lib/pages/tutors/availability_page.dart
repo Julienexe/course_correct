802006b5a4e4aa6399c301893d7617daa1105f09
Michael Rusezera <michaelrusezera@gmail.com>
2024-08-12 23:42:40 +0300

    add availability page

---
+++ lib/pages/tutors/availability_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AvailabilityPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Availability'),
+      ),
+      body: Center(
+        child: Text(
+          'Coming Soon...',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
