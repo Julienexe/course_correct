b51f1608592903706847ea5058fabd37d96f9a59
Michael Rusezera <michaelrusezera@gmail.com>
2024-08-12 23:46:47 +0300

    create appointments page

---
+++ lib/pages/tutors/appointments.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppointmentsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appointments'),
+      ),
+      body: Center(
+        child: Text(
+          'Coming Soon...',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
